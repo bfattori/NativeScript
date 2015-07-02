4e38673dec2c27114f5b7a317414bc1d5500fc17
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-07-02 10:38:35 +0300

    Add missing typescript declarations, according to TypeScript 1.5.x

--- declarations.d.ts
+++ declarations.d.ts
@@ -2,6 +2,22 @@
 
 declare function log3(msg): void;
 
+interface TypedPropertyDescriptor<T> {
+    enumerable?: boolean;
+    configurable?: boolean;
+    writable?: boolean;
+    value?: T;
+    get?: () => T;
+    set?: (value: T) => void;
+}
+
+declare type ClassDecorator = <TFunction extends Function>(target: TFunction) => TFunction | void;
+declare type PropertyDecorator = (target: Object, propertyKey: string | symbol) => void;
+declare type MethodDecorator = <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>) => TypedPropertyDescriptor<T> | void;
+declare type ParameterDecorator = (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
+
+//-----------
+
 interface Console {
     time(reportName: string): void;
     timeEnd(reportName: string): void;
